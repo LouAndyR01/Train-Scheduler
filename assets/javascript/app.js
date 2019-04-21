101ee61559319c6743c15358fbef9f467ba84a91
Louis Rodriguez <louisp285@yahoo.com>
2019-04-20 20:17:56 -0500

    submit hw

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,13 +15,25 @@
         messagingSenderId: "700791304460"
     };
 
-    //  Initializae Firebase
-    firebase.initializeApp(config);
+    //  Initializae Firebase //
+        firebase.initializeApp(config);
 
     var database = firebase.database();
 
-      //  adding new train info to the table //
-      $("#add-train-btn").on("click", function() {
+    // Display for the current time //
+    var currentTime = null;
+    
+        function updateTime() {
+        currentTime = moment().format("HH:mm:ss");
+        $("#currentTime").html(currentTime);
+    }
+        $(document).ready(function() {
+        updateTime();
+        setInterval(updateTime, 1000);
+    });
+
+    //  adding new train info to the table //
+       $("#add-train-btn").on("click", function() {
         event.preventDefault();
 
     // pulling info from the bootstrap input forms //
@@ -29,8 +41,8 @@
     var trainDestination = $("#userDestination").val().trim();
     var trainFirstTime = $("#firstTimeInput").val().trim();
     var trainFrequency = $("#trainFrequency").val().trim();
-
-    // holding train data //
+    
+    // get values for the input fields //
     var newTrain = {
         name: trainName,
         destination: trainDestination,
@@ -39,50 +51,51 @@
     };
 
     // push train data to database //
-    database.ref().push(newTrain);
+        database.ref().push(newTrain);
 
         alert("Your train has been added");
 
     // reset bootstrap text forms //
         $("#userTrainName").val("");
         $("#userDestination").val("");
-        $("#firstTimeInput").val("");
         $("#trainFrequency").val("");
+        $("#firstTimeInput").val("");   
     });
 
-    database.ref().on("child_added", function(childSnapshot) {
+        database.ref().on("child_added", function(childSnapshot) {
        
     // values for the new variables //
     var trainName = childSnapshot.val().name;
     var trainDestination = childSnapshot.val().destination;
-    var trainFirstTime = childSnapshot.val().firstTime;
     var trainFrequency = childSnapshot.val().frequency;
+    var trainFirstTime = childSnapshot.val().firstTime;
+
   
-     // moment js, for formated time //
+    // moment js, for formated time //
     var trainTimeCalc = moment(trainFirstTime, "HH:mm").subtract(1, "years");
 
-
     // calculate difference, between start time and current time //
-    var diffTime = moment().diff(moment(trainTimeCalc), "minutes");
+    var timeDifference = moment().diff(moment(trainTimeCalc), "minutes");
 
     // calculates the remaining minutes //
-    var tRemainder = diffTime % trainFrequency;
+    var tRemainder = timeDifference % trainFrequency;
 
     // the next train arrival time in minutes //
-    var minAway = trainFrequency - tRemainder;
+    var minNextTrain = trainFrequency - tRemainder;
 
     // the next arrival time of train
-    var nexTArrival = moment().add(minAway, "minutes");
-    var nextTrainArrival = moment(nexTArrival).format("hh:mm");
+    var nextArrival = moment().add(minNextTrain, "minutes");
+    var nextTrainArrival = moment(nextArrival).format("hh:mm");
 
     // append the rows of data for the new train data //
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDestination),
-        $("<td>").text(trainFrequency),
-        $("<td>").text(nextTrainArrival),
-        $("<td>").text(minAway),  
+        $("<td class='text-center'>").text(trainFrequency),
+        $("<td class='text-center'>").text(nextTrainArrival),
+        $("<td class='text-center'>").text(minNextTrain), 
     );
         $("#train-table > tbody").append(newRow);   
-    });
+    });    
 
+ 
